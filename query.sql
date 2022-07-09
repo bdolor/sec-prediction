2a99d0a6b80b99a9a3d55189719c0dbf76935dc3
bdolor <brad@bradpayne.ca>
2022-07-09 13:54:24 -0700

    format sql, add optional skills

--- query.sql
+++ query.sql
@@ -1,27 +1,27 @@
-with opps as (select "opportunities"."id",
+with opps as (select opportunities.id,
                      (CASE
                           WHEN versions."createdAt" > statuses."createdAt" THEN versions."createdAt"
                           ELSE statuses."createdAt" END) AS "updatedAt",
                      versions.id                         as "versionsId",
                      versions."createdAt"                as "versionsCreatedAt",
                      versions."totalMaxBudget",
-                     "versions"."minTeamMembers",
-                     "versions"."mandatorySkills",
+                     versions."minTeamMembers",
+                     versions."mandatorySkills",
                      versions."optionalSkills",
                      versions."questionsWeight",
-                     "versions"."codeChallengeWeight",
-                     "versions"."scenarioWeight",
-                     "versions"."priceWeight"
-              from "swuOpportunities" as "opportunities"
+                     versions."codeChallengeWeight",
+                     versions."scenarioWeight",
+                     versions."priceWeight"
+              from "swuOpportunities" as opportunities
                        inner join
-                   "swuOpportunityStatuses" as "statuses" on "opportunities"."id" = "statuses"."opportunity" and
-                                                             "statuses"."createdAt" = (select max("createdAt")
+                   "swuOpportunityStatuses" as statuses on opportunities.id = statuses.opportunity and
+                                                             statuses."createdAt" = (select max("createdAt")
                                                                                        from "swuOpportunityStatuses" as statuses2
                                                                                        where statuses2.opportunity = opportunities.id
                                                                                          and statuses2.status is not null)
                        inner join
-                   "swuOpportunityVersions" as "versions" on "opportunities"."id" = "versions"."opportunity" and
-                                                             "versions"."createdAt" = (select max("createdAt")
+                   "swuOpportunityVersions" as versions on opportunities.id = versions.opportunity and
+                                                             versions."createdAt" = (select max("createdAt")
                                                                                        from "swuOpportunityVersions" as versions2
                                                                                        where versions2.opportunity = opportunities.id)),
      props as (select proposals.opportunity,
@@ -35,14 +35,14 @@ with opps as (select "opportunities"."id",
                       proposals."scenarioScore",
                       proposals."priceScore",
                       sum(responses.score)                as teamquestions,
-                      statuses."status"
-               from "swuProposals" as "proposals"
+                      statuses.status
+               from "swuProposals" as proposals
                         INNER JOIN
-                    "swuTeamQuestionResponses" as responses on "responses"."proposal" = "proposals"."id"
+                    "swuTeamQuestionResponses" as responses on responses.proposal = proposals.id
                         INNER JOIN
-                    "swuProposalStatuses" as "statuses"
-                    on "proposals"."id" = "statuses"."proposal" and "statuses"."status" is not null and
-                       "statuses"."createdAt" = (select max("createdAt")
+                    "swuProposalStatuses" as statuses
+                    on proposals.id = statuses.proposal and statuses.status is not null and
+                       statuses."createdAt" = (select max("createdAt")
                                                  from "swuProposalStatuses" as statuses2
                                                  where statuses2.proposal = proposals.id
                                                    and statuses2.status is not null)
